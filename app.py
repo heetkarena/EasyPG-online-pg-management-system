a7be563f29e3a4085cc77f86e3c6be0c2c2c9b14
heet_karena <heetkarena111@gmail.com>
2026-03-10 20:06:49 +0530

    small change in app.py

--- app.py
+++ app.py
@@ -733,4 +733,6 @@ def internal_error(error):
     return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT'))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    
